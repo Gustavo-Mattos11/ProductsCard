1e8d6e4fd4966e564aea09aeeaf240b2bfbe7475
GustavoMattos11 <gustavonunesdemattos@outlook.com>
2025-03-09 20:23:55 -0300

    inclusão e exclusão do carrinho pt-1

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+const products = 0
+
+productList = [
+    {
+        name: 'Waffle',
+        value: 0
+    },
+    {}
+]
+
 function mapear(contentList) {
-    console.log(window.innerWidth)
     contentList.map((item) => {
         createElements(item)
     })
@@ -7,11 +16,8 @@ function mapear(contentList) {
 async function objetosJSON() {
     try {
         const resposta = await fetch('data.json')
-        console.log(resposta)
         const contentList = await resposta.json()
-        console.log(contentList)
         mapear(contentList)
-        console.log(contentList[0])
 
     } catch (erro) {
         console.error("Erro ao carregar o arquivo JSON:", erro)
@@ -19,64 +25,81 @@ async function objetosJSON() {
 
 }
 
-function cart(){
+function cart() {
+    const centerColumn = document.getElementById('centerColumn')
     const cartDiv = document.createElement('div')
     cartDiv.className = 'cartDiv'
+
+
     const cartColumn = document.getElementById('cart')
     const titleCard = document.createElement('p')
     titleCard.innerHTML = 'Your Cart (0)'
-    
+
     const insideCart = document.createElement('div')
     const img = document.createElement('img')
     img.src = "./assets/images/illustration-empty-cart.svg"
     const descriptionCard = document.createElement('p')
     descriptionCard.innerHTML = 'Your added items will appear here'
-    
-    cartColumn.appendChild(cartDiv)
+    if (window.innerWidth <= 425) {
+        const footer = document.createElement('footer')
+
+        centerColumn.appendChild(footer)
+        footer.appendChild(cartDiv)
+
+
+
+    } else {
+        const main = document.querySelector('main')
+        const aside = document.createElement('aside')
+        main.insertBefore(aside, centerColumn)
+        cartColumn.appendChild(cartDiv)
+    }
+
     cartDiv.appendChild(titleCard)
     cartDiv.appendChild(insideCart)
     insideCart.appendChild(img)
     insideCart.appendChild(descriptionCard)
 
-
 }
 
 function createElements(item) {
     const productColumn = document.getElementById('productColumn')
-    const productDiv = document.createElement('productDiv')
+    const productDiv = document.createElement('div')
     productDiv.className = 'productDiv'
 
     const divImage = document.createElement('div')
     divImage.className = 'divImage'
-    
+
     const productButton = document.createElement('button')
-    productButton.className = 'imageButton'
+    productButton.className = 'productButton'
+    productButton.name = item.category
+
 
     const productText = document.createElement('div')
     productText.className = 'productText'
     const paragraphCart = document.createElement('p')
     paragraphCart.innerHTML = 'Add to Cart'
 
-    const addCart = document.createElement('img')
-    addCart.src = ' ./assets/images/icon-add-to-cart.svg'
+    const imgCart = document.createElement('img')
+    imgCart.src = ' ./assets/images/icon-add-to-cart.svg'
 
     const category = document.createElement('p')
     category.innerHTML = item.category
-    
-    
+
+
     const name = document.createElement('p')
     name.innerHTML = item.name
     name.className = 'name'
-    
+
     const img = document.createElement('img')
     img.className = 'img'
-    
+
     const price = document.createElement('p')
     const formattedPrice = parseFloat(item.price).toFixed(2)
     price.innerHTML = `$${formattedPrice}`
     price.style = 'color: orange;'
-    
-    
+
+
     if (window.innerWidth <= 479) {
         img.src = item.image.mobile
     } else if (window.innerWidth > 479 && window.innerWidth <= 768) {
@@ -85,7 +108,7 @@ function createElements(item) {
         img.src = item.image.desktop
     }
 
-    productButton.appendChild(addCart)
+    productButton.appendChild(imgCart)
     productButton.appendChild(paragraphCart)
     productDiv.appendChild(divImage)
     productDiv.appendChild(productText)
@@ -96,4 +119,32 @@ function createElements(item) {
     productText.appendChild(price)
     productColumn.appendChild(productDiv)
 
+    productButton.onclick = () => {
+        if (productButton.childElementCount < 3) {
+            var products = 0
+            paragraphCart.innerHTML = products
+            const removeCart = document.createElement('img')
+            removeCart.src = './assets/images/icon-decrement-quantity.svg'
+            removeCart.onclick = () => {
+                if(products >= 1){
+                    products--
+                }
+                paragraphCart.innerHTML = products // somar ou diminuir quantidade ao clicar em um dos botões
+            }
+            const addCart = document.createElement('img')
+            addCart.src = './assets/images/icon-increment-quantity.svg'
+            addCart.onclick = () => {
+                products++
+                paragraphCart.innerHTML = products // somar ou diminuir quantidade ao clicar em um dos botões
+            }
+            productButton.classList.add('quantity')
+            
+            productButton.insertBefore(removeCart, paragraphCart)
+            productButton.appendChild(addCart)
+            productButton.removeChild(imgCart)
+        } else {
+            
+        }
+
+    }
 }
